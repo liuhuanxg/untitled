03c707d733c98f58b9ca99a6b42bff45b176ce5e
liuhuan <18737307883@sina.cn>
2019-04-04 15:21:31 +0800

    delet somethins

--- 入门到实践/图表/rw_visual.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-from  random_walk import  RandomWalk
-rw=RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values,rw.y_values,c='red',s=5)
-plt.show()
--- 第一阶段/银行系统1/ATM.py
+++
@@ -1,6 +0,0 @@
-for i in range(5):
-    if i>3:
-        print(i,end=' ')
-        break
-else:
-         print('西瓜')
