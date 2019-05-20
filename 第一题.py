80189909015968089e6cf45e8628f25f3e158e9f
liuhuan <18737307883@sina.cn>
2019-05-20 14:53:36 +0800

    add page

--- 第一题.py
+++ 第一题.py
@@ -1,9 +1,16 @@
-#现有两个从小到大排好序的 int 数组（每个数组自身没有重复元素）。请找出所有在 这两个数组中都出现过的数。请写一个函数，输入为两个数组。
+import os
+import sys
 
-def choice(list1,list2):
-    print(list(set(list1)&set(list2)))
-
+def dump_file(input_dir):
+    if not os.path.exists(input_dir):
+        print('dir {} not exists'.format(input_dir))
+        return
+    path_list = [os.path.join(input_dir, f).strip('\n') for f in os.listdir(input_dir)]
+    file_path_list = filter(lambda file_path: os.path.isfile(file_path), path_list)
+    for file_path in file_path_list:
+        print('{}:{} size:{}'.format(os.getpid(), file_path, os.path.getsize(file_path)))
 if __name__ == '__main__':
-    list1=eval(input('请输入第一个列表：'))
-    list2=eval(input('请输入第二个列表：'))
-    choice(list1,list2)
+    if len(sys.argv) > 1:
+        dump_file(sys.argv[1])
+    else:
+        dump_file(os.getcwd())
--- 第三题.py
+++ 第三题.py
@@ -1,51 +1,23 @@
-import itertools
-
-def compare(str1,str2):
-    r1,flag1=check(str1)
-    r2,flag2=check(str2)
-    result=0
-    if flag1 and flag2:
-        if r1>r2:
-            result=1
-        elif r1<r2:
-            result=-1
-        elif r1==r2:
-            result=0
-    elif flag1 or flag2:
-        if flag1:
-            result=1
-        elif flag2:
-            result=-1
-    else:
-        for i in range(len(r1)):
-            if r1[i]>r2[i]:
-                result=1
-                break
-            elif r1[i]<r2[i]:
-                result=-1
-                break
-    print(result)
-
-def check(str):
-    arr=[]
-    for i in str:
-        if i in first_list:
-            i=10
-        elif i in secon_list:
-            i=1
-        arr.append(int(i))
-    arr=sorted(arr)
-    mylist = list(itertools.permutations(arr, 3))
-    for j in mylist:
-        if (j[0]+j[1]+j[2])% 10 == 0:
-            other_list=list(set(list(j)) & set(list(arr)))
-            x=(other_list[0]+other_list[1])%10
-            return x,True
-    return arr,False
+import os
+import sys
+import multiprocessing
+def dump_file(input_dir):
+    if not os.path.exists(input_dir):
+        print('dir {} not exists'.format(input_dir))
+        return
+    path_list = [os.path.join(input_dir, f).strip('\n') for f in os.listdir(input_dir)]
+    file_path_list = filter(lambda file_path: os.path.isfile(file_path),path_list)
+    for file_path in file_path_list:
+        print('{}:{} size:{}'.format(os.getpid(), file_path,os.path.getsize(file_path)))
 
 if __name__ == '__main__':
-    first_list=['T','J','K','t','j','k','q','Q',]
-    secon_list=['A','a']
-    str1=input('请输入第一幅牌:')
-    str2=input('请输入第一幅牌:')
-    compare(str1,str2)
+    pool_size = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=pool_size,)
+    for i in range(pool_size):
+        if len(sys.argv) > 1:
+            pool.apply_async(dump_file, args=(sys.argv[1],))
+        else:
+            pool.apply_async(dump_file, args=(os.getcwd(),))
+    pool.close()
+    pool.join()
+
